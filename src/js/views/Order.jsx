528be30eba227d8f8105d903db1a64f310445acd
Elliot Sabitov <elliotsabitov@gmail.com>
2017-09-30 01:47:46 -0400

    Created order view files

---
+++ src/js/views/Order.jsx
@@ -0,0 +1,17 @@
+class Register extends React.Component {
+  constructor (props) {
+    super(props);
+
+  }
+
+  componentDidMount(){
+  }
+
+  render() {
+    return (
+      <div className="order fx-column fx-center">
+        Order
+      </div>
+    );
+  }
+}
