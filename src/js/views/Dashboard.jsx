304f2114bc6cbf2e9e707640b0cb1e1ce262e6fc
Elliot Sabitov <elliotsabitov@gmail.com>
2017-09-30 01:19:43 -0400

    made a still recording gif

--- src/js/views/Dashboard.jsx
+++ src/js/views/Dashboard.jsx
@@ -17,6 +17,7 @@ class Dashboard extends React.Component {
   }
 
   beginRecording() {
+    store.dispatch({ type: UPDATE_SESSION, payload: { recording: true } });
     // var __object = { "user": null, "restaurants": [{ "apiKey": "831648df8695e587144247b91fbcb2f3d548bfb0fbc2a739", "name": "Big Al's Pizza", "logoUrl": "https://eatstreet-static.s3.amazonaws.com/assets/images/restaurant_logos/big-als-pizza-21407_1407868358668.png", "latitude": 40.708958, "longitude": -74.011858 }, { "apiKey": "831648df8695e587fe2bbabe6b94d9a6ca2d8b721b5ea143", "name": "Bleecker Street Pizza", "logoUrl": "https://eatstreet-static.s3.amazonaws.com/assets/images/restaurant_logos/bleecker-street-pizza-8433_1429825758252.png", "latitude": 40.732184, "longitude": -74.003442 }, { "apiKey": "831648df8695e587884225aec137a02a945357f5e0c10306", "name": "Cafe Amore's Pizzeria Ristorante", "logoUrl": "https://eatstreet-static.s3.amazonaws.com/assets/images/restaurant_logos/cafe-amores-pizzeria-ristorante-17057_1403807111709.png", "latitude": 40.715726, "longitude": -74.009698 }, { "apiKey": "831648df8695e587fe2bbabe6b94d9a653f6cb07fa20c520", "name": "Chelsea Ristorante", "logoUrl": "https://eatstreet-static.s3.amazonaws.com/assets/images/restaurant_logos/chelsea-ristorante-8536_1392768035995.png", "latitude": 40.740646, "longitude": -74.00166 }, { "apiKey": "831648df8695e587884225aec137a02aae4b68948f9c5f8c", "name": "Clinton Square Pizza", "logoUrl": "https://eatstreet-static.s3.amazonaws.com/assets/images/restaurant_logos/clinton-square-pizza-16694_1403128417086.png", "latitude": 40.713946, "longitude": -73.98722 }] };
     // this.props.updateRestaurants(__object.restaurants);
     voiceService.startSpeechToText(this.onTranscript, 'Please tell us what you would like!');
@@ -37,18 +38,21 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashboard mdl-layout__content fx-column fx-center">
+        <div className="recording">
+          <img src="/assets/images/recording.gif" />
+          <img src="/assets/images/recording.png" className={"still " + (store.getState().app.recording ? 'hidden' : 'show')}/>
+        </div>
         <div>
           <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect" onClick={this.beginRecording}>Start</button>
         </div>
-        <div className="recording">
-          <img src="/assets/images/recording.gif" />
-        </div>
+        {/*
         <div className="page-content">
           Begin Recording
           <br />Lat: {this.gps.lat}
           <br />Lng: {this.gps.lng}
           <br />Heading: {this.gps.heading}
         </div>
+        */}
       </div>
     );
   }
