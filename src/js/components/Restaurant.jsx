7c2fafb18a808a98ea68f8d4e7f14a1585c40107
Julian Offermann <julian.offermann@sulzer-us.com>
2017-09-29 20:09:34 -0400

    added logic

--- src/js/components/Restaurant.jsx
+++ src/js/components/Restaurant.jsx
@@ -2,14 +2,13 @@ class Restaurant extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
     }
 
     render() {
         return <div className="app-restaurant-item">
             <div>
                 <img src={this.props.item.logoUrl} />
-                <h5>{this.props.item.name}</h5>
+                <h5><b>{this.props.index}.</b>&nbsp;{this.props.item.name}</h5>
             </div>
         </div>;
     }
